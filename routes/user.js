7e0f320799096071f45e0d63d7b2f8f4c4fddb16
Meisam Nasiri <meisam.nasiri74@gmail.com>
2023-07-03 16:44:09 +0330

    Updated user APIs and Repository

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,41 @@
 const express = require("express");
-const { registerUser } = require("../user/userRepository");
+const {
+  registerUser,
+  deleteUser,
+  updateUser,
+  findUser,
+} = require("../user/userRepository");
 
 const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await findUser(id);
+  res.status(200).send(user);
+});
 
 router.post("/", async (req, res) => {
   const obj = req.body;
   const user = await registerUser(obj);
   res.status(200).send(user);
+});
+
+router.delete("/:id", async (req, res) => {
+  const userId = req.params.id;
+  const result = await deleteUser(userId);
+  res.status(200).send(result);
+});
+
+router.put("/", async (req, res) => {
+  const userId = req.body.id;
+  const userUpdate = {
+    _id: req.body._id,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const result = await updateUser(userUpdate);
+  res.status(200).send(result);
 });
 
 module.exports = router;
--- user/userRepository.js
+++ user/userRepository.js
@@ -1,6 +1,10 @@
 const User = require("./userModel");
 const bcrypt = require("bcrypt");
 
+async function findUser(id) {
+  const user = await User.findById(id);
+}
+
 async function registerUser(obj) {
   let user = new User({
     name: obj.name,
@@ -20,6 +24,30 @@ async function registerUser(obj) {
   };
 }
 
+async function deleteUser(userId) {
+  const result = await User.deleteOne({ _id: userId });
+  return result;
+}
+
+async function updateUser(userUpdate) {
+  const salt = bcrypt.genSalt(10);
+  const hashed = bcrypt.hash(userUpdate.password, salt);
+
+  const result = await User.updateOne(
+    { _id: userUpdate._id },
+    {
+      name: userUpdate.name,
+      email: userUpdate.email,
+      password: hashed,
+    }
+  );
+
+  return result;
+}
+
 module.exports = {
+  findUser,
   registerUser,
+  deleteUser,
+  updateUser,
 };
