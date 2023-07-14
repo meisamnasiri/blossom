93d0591970ad78c91794caad699f8f4edd89f044
Meisam Nasiri <meisam.nasiri74@gmail.com>
2023-07-14 14:28:46 +0330

    Finished the reminder module for on time reminder.

--- reminder/reminder.js
+++ reminder/reminder.js
@@ -5,18 +5,15 @@ module.exports = async function reminder() {
     hasArrived: false,
     dueDate: { $ne: null },
   });
+  console.log("todo list is: ", todoList.length);
 
-  for (const [index, todo] of Object.entries(todoList)) {
+  for (const todo of todoList) {
     const dueDate = todo.dueDate;
     if (dueDate && dueDate.getTime() <= Date.now()) {
       console.log(`Time to ${todo.task}`);
-      const updatedTodo = await Todo.findOneAndUpdate(
-        { _id: todo._id },
-        { hasArrived: true },
-        { new: true }
-      );
+      await Todo.findOneAndUpdate({ _id: todo._id }, { hasArrived: true });
     }
   }
 
-  setTimeout(reminder, 6000);
+  setTimeout(reminder, 1000);
 };
